1a9eb56feca19b0064d5dcbdc8b1e4e570b22465
redboo <garda1@mail.ru>
2023-03-23 06:30:00 +0500

    оптимизация parse_youtube_url

--- utils/parse_url.py
+++ utils/parse_url.py
@@ -1,44 +1,38 @@
 import re
 
+REGEX_PATTERNS = {
+    "channel_id": re.compile(r"^[a-zA-Z0-9_-]{24}$"),
+    "channel_url": re.compile(r"(?:(?:https?:)?//)?(?:www\.)?youtube.com/channel/([a-zA-Z0-9_-]+)"),
+    "video_url": re.compile(r"(?:(?:https?:)?//)?(?:www\.)?youtube.com/watch\?v=([a-zA-Z0-9_-]+)"),
+    "video_id": re.compile(r"^[a-zA-Z0-9_-]{11}$"),
+    "channel_name": re.compile(r"(?:(?:https?:)?//)?(?:www\.)?youtube.com/c/([^/\s?]+)"),
+    "user_name": re.compile(r"^(?:https?:\/\/(?:www\.)?youtube\.com\/)?(?:@)?([a-zA-Z0-9_-]+)$"),
+}
+
 
 def parse_youtube_url(url_or_id):
-    # Определение шаблонов URL для каналов, видео и пользователей
-    patterns = [
-        (re.compile(r"(?:(?:https?:)?//)?(?:www\.)?youtube.com/channel/([a-zA-Z0-9_-]+)"), "channel"),
-        (re.compile(r"(?:(?:https?:)?//)?(?:www\.)?youtube.com/watch\?v=([a-zA-Z0-9_-]+)"), "video"),
-        (re.compile(r"^[a-zA-Z0-9_-]{11}$"), "video"),
-        (re.compile(r"(?:(?:https?:)?//)?(?:www\.)?youtube.com/c/([^/\s?]+)"), "channel_name"),
-        (re.compile(r"^(?:https?:\/\/(?:www\.)?youtube\.com\/)?(?:@)?([a-zA-Z0-9_-]+)$"), "user_name"),
-        (re.compile(r"^[a-zA-Z0-9_-]{24}$"), "channel_id"),
-    ]
-
-    # Поиск совпадений в URL-адресе или ID
-    for pattern, data_type in patterns:
+    for data_type, pattern in REGEX_PATTERNS.items():
         match = pattern.search(url_or_id)
         if match:
-            return data_type, match.group(1)
-
+            if data_type == "channel_url":
+                return "channel_id", match.group(1)
+            elif data_type == "channel_id":
+                return data_type, url_or_id
+            else:
+                return data_type, match.group(1)
     return None, None
 
 
 if __name__ == "__main__":
-    # Пример использования
-    url = "https://www.youtube.com/@theextensional"
-    data_type, data = parse_youtube_url(url)
-    print(data_type, data)
+    urls = [
+        "https://www.youtube.com/@theextensional",  # пользователь
+        "https://www.youtube.com/watch?v=dQw4w9WgXcQ",  # видео
+        "https://www.youtube.com/channel/UCrV_cFYbUwpjSOPVJOjTufg",  # канал ID
+        "https://www.youtube.com/c/Экстенсиональный",  # название канала
+        "UCrV_cFYbUwpjSOPVJOjTufg",  # канал ID
+        "@theextensional",  # пользователь
+    ]
 
-    url = "https://www.youtube.com/channel/UCrV_cFYbUwpjSOPVJOjTufg"
-    data_type, data = parse_youtube_url(url)
-    print(data_type, data)
-
-    url = "https://www.youtube.com/c/Экстенсиональный"
-    data_type, data = parse_youtube_url(url)
-    print(data_type, data)
-
-    url = "UCrV_cFYbUwpjSOPVJOjTufg"
-    data_type, data = parse_youtube_url(url)
-    print(data_type, data)
-
-    url = "@theextensional"
-    data_type, data = parse_youtube_url(url)
-    print(data_type, data)
+    for url in urls:
+        data_type, data = parse_youtube_url(url)
+        print(f"URL: {url}, Тип данных: {data_type}, Данные: {data}")
