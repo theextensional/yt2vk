d3a50d864707cf8e52bf29cd9186ff0604ded945
redboo <garda1@mail.ru>
2023-03-23 08:53:53 +0500

    remove unused file

--- pytutube.py
+++
@@ -1,5 +0,0 @@
-from pytube import Channel
-
-# c = Channel('https://www.youtube.com/c/ProgrammingKnowledge')
-c = Channel("https://www.youtube.com/@Max_Katz")
-print(f"Downloading videos by: {c.channel_name}")
